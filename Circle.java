7d780af466388583a6ed6ed112abcedfadfb714c
sagarsimha <sagar.abhay.simha@gmail.com>
2019-05-06 21:20:23 +0200

    Second Exercise

--- Circle.java
+++ Circle.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+import java.lang.Math.*;
 
 public class Circle {
 	//todo: add some methods to compute a circle's diameter, circumference and area.
 	//print some outputs to confirm your methods are working correctly.
+
+	public static void main(String[] args) {
+		
+		System.out.println("Please enter the radius of the circle");
+		Scanner scan = new Scanner(System.in);
+
+		float radius = scan.nextFloat();
+
+		System.out.println("Radius of the circle is: " + radius + " cm");
+		System.out.println("Diameter of the circle is: " + Diameter(radius) + " cm");
+		System.out.println("Circumference of the circle is: " + Circumference(radius) + " cm");
+		System.out.println("Area of the circle is: " + Area(radius) + " sq.cm");
+	}
+
+	public static float Diameter(float radius){
+		float diameter_ = 2 * radius;
+		return diameter_;
+	}
+
+	public static double Circumference(float radius){
+		double circumference_ = 2 * Math.PI * radius;
+		return circumference_;
+	}
+
+	public static double Area(float radius){
+		double area_ = Math.PI * radius * radius;
+		return area_;
+	}
+	
+
 }
--- Prime.java
+++ Prime.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 
 public class Prime {
-		
+
 	//todo: write a method isPrime which tells you if a given number is a prime or not
 	//then, write another method findDoublette which takes an integer value as input and 
 	//is looking for the first prime-doublette it findes. A prime-doublette consists of
@@ -10,6 +10,59 @@ public class Prime {
 	//The function should return the smaller one of the doublette. If there exists no
 	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
 	//the function should return 0.
-		
+
+	public static void main(String[] args) {
+
+		System.out.println("Please enter a POSITIVE integer number");
+		Scanner scan = new Scanner(System.in);
+		int number_ = scan.nextInt();
+		if(isPrime(number_)){
+			System.out.println("The number is Prime");
+		}
+		else{
+			System.out.println("The number is NOT Prime");
+		}
+		System.out.println("The smaller of the first-doublette from (including) "+ number_ + " is " + findDoublette(number_)) ;
+	}
+
+		// A prime is a number that has only two factors, 1 and the number itself
+	public static boolean isPrime(int number){
+
+		if((number ==1) || (number ==2)){
+			return true;
+		}
+		else{
+			if(!checkprime(number) || number==0){
+				return false;
+			}
+			else{
+				return true;	
+			}
+		}
+	}
+
+	public static boolean checkprime(int n){
+		boolean isprime_=true;
+		for(int i=2; i<n; i++){
+			if(n%i == 0){
+				isprime_ = false;
+			}
+		}
+		return isprime_;
+
+	}
+
+	public static int findDoublette(int number){
+		for(int i=number; i<=Integer.MAX_VALUE-2; i++){
+			if(isPrime(i)){
+				if( isPrime(i+2) ){
+					return i;
+				}
+			}
+		}
+		return 0;			
+
+	}
+
 	
 }
